a5a85b1ba9010e74a1b6e7ff62bb054a8d5c5a24
eggfryed <[200~146197827+eggfryed@users.noreply.github.com~>
2025-02-07 16:08:50 +0000

    3 of 6 steps reached
    use switch statements for user input

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,38 @@
-<script>
-    
-</script>
+console.log("hello world!");
+//computer choice
+function getComputerChoice() {
+    let choice = (Math.floor(Math.random()*100));
+
+    if (choice <= 30) {
+        return ("The computer played Rock!");
+
+    } else if (choice >= 31 && choice <= 60) {
+        return ("The computer played Paper!");
+
+    } else if (choice >= 61 && choice <= 100) {
+        return ("The computer played Scissors!");
+    }
+}
+//computer choice output
+console.log(getComputerChoice());
+
+//human choice
+function getHumanChoice() {
+
+switch (prompt()) {
+    case "rock":
+        return "rock";
+        break;
+    case "paper":
+        return "paper";
+        break;
+    case "scissors":
+        return "scissors";
+        break;
+    default:
+        return "not an option";
+    }
+}
+
+//numan choice output
+console.log(getHumanChoice());
